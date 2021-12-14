6046dcb33420ebee43f2cdd8c4ad65fa0f3e8a41
Axon_BlAzE Ezz <81565434+AxonBlAzE@users.noreply.github.com>
2021-12-14 20:47:10 +0000

    minor changes

--- main.py
+++ main.py
@@ -22,20 +22,25 @@ async def on_message(message):
         await message.channel.send('Hello World')
 
     if message.content.startswith('!creators'):
-        await message.channel.send('Shreyans Mulkutkar and Pratik Thakare')
+        await message.channel.send("```Shreyans Mulkutkar and Pratik Thakare```")
 
     if message.content.startswith('!grindset'):
         # grab the user who sent the command
         user=message.author
         voice_channel=user.voice.channel
         
-        # only play music if user is in a voice channel
+        # only play music if user is in a voice channel and bot is not in the voice channel
         if voice_channel != None:
             # grab user's voice channel
-            await message.channel.send('User is in channel: '+ voice_channel.name)
+            await message.channel.send('```Sigma Rule #0: Turn that Mindset into Grindset```')
 
             vc = await voice_channel.connect()
             vc.play(discord.FFmpegPCMAudio('Grindset.mp3'), after=lambda e: print('done', e))
+        
+        # only play music if user is and the bot is already in a voice channel
+        elif voice_channel == user.voice.channel:
+          vc.play(discord.FFmpegPCMAudio('Grindset.mp3'), after=lambda e: print('done', e))
+          
         else:
             await message.channel.send('User is not in a channel.')
 
