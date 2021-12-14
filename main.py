1bb10d8d61b8c367133e04bdba8ff55e3fe9d4e5
Axon_BlAzE Ezz <81565434+AxonBlAzE@users.noreply.github.com>
2021-12-14 19:12:15 +0000

    completed grindset

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import discord
 import os
 from music import Music 
 from grind import Grind
+import asyncio
 
 my_secret = os.environ['myToken']
 client = discord.Client()
 voice = None
 music = Music(client)
+mygrind = Grind(client)
 
 @client.event
 async def on_ready():
@@ -17,17 +19,25 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!hello-world'):
+        await message.channel.send('Hello World')
 
     if message.content.startswith('!creators'):
         await message.channel.send('Shreyans Mulkutkar and Pratik Thakare')
-    
+
     if message.content.startswith('!grindset'):
-        voice = discord.VoiceClient(client, message.author)
-        await message.channel.send('Sigma rule #0: turn that mindset into grindset')
-        grind.play(message)
-        # music.play()
-        # await message.channel.send(file=discord.File('Grindset.mp3'))
+        # grab the user who sent the command
+        user=message.author
+        voice_channel=user.voice.channel
+        
+        # only play music if user is in a voice channel
+        if voice_channel != None:
+            # grab user's voice channel
+            await message.channel.send('User is in channel: '+ voice_channel.name)
+
+            vc = await voice_channel.connect()
+            vc.play(discord.FFmpegPCMAudio('Grindset.mp3'), after=lambda e: print('done', e))
+        else:
+            await message.channel.send('User is not in a channel.')
 
 client.run(my_secret)
