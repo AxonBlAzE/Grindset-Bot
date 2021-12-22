e554e8bcb929da8e59775af82e64b2a44e4a26d8
Axon_BlAzE Ezz <81565434+AxonBlAzE@users.noreply.github.com>
2021-12-22 08:10:12 +0000

    fixes

--- main.py
+++ main.py
@@ -51,25 +51,46 @@ async def on_message(message):
         await message.channel.send(
             "```Shreyans Mulkutkar and Pratik Thakare```")
 
+    #doest work
+    # if message.content.startswith('!grindset'):
+    #     user_voice = message.author.voice
+    #     if not user_voice:
+    #         await message.channel.send("You need to be connected in a voice channel to use this command!")
+    #         return
+
+    #     # This allows for more functionality with voice channels
+    #     voice = discord.utils.get(bot.voice_clients, guild=message.guild)
+        
+    #     # None being the default value if the bot isnt in a channel (which is why the is_connected() is returning errors)
+    #     if voice == None:
+    #         voice = await user_voice.channel.connect()
+        
+    #     voice.play(
+    #         audio,
+    #         after=lambda e: disconnect(message)
+    #     )
+
     if message.content.startswith('!grindset'):
-        user_voice = message.author.voice
-        if not user_voice:
-            await message.channel.send("You need to be connected in a voice channel to use this command!")
-            return
+      # grab the user who sent the command
+      user=message.author
+      voice_channel=user.voice.channel
+      
+      # only play music if user is in a voice channel and bot is not in the voice channel
+      if voice_channel != None:
+          # grab user's voice channel
 
-        # This allows for more functionality with voice channels
-        voice = discord.utils.get(bot.voice_clients, guild=message.guild)
+        await message.channel.send('```Sigma Rule #0: Turn that Mindset into Grindset```')
+
+        vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio('Grindset.mp3'), after=lambda e: print('', e))
+      
+      # only play music if user is and the bot is already in a voice channel
+      elif voice_channel == user.voice.channel:
+        vc.play(discord.FFmpegPCMAudio('Grindset.mp3'), after=lambda e: print('', e))
         
-        # None being the default value if the bot isnt in a channel (which is why the is_connected() is returning errors)
-        if voice == None:
-            voice = await user_voice.channel.connect()
+      else:
+        await message.channel.send('User is not in a channel.')
         
-        voice.play(
-            audio,
-            after=lambda e: disconnect(message)
-        )
-
-
     if message.content.startswith('!rules'):
         s = db.keys()
         l = list(s)
